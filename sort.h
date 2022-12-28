3a435af73e754ecc92426efffd70c4a3161a3ee3
mavix21 <marcevizcarra2109@hotmail.com>
2022-12-27 21:55:45 -0500

    Fix betty

--- sort.h
+++ sort.h
@@ -5,19 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
+/**
  * struct listint_s - Double linked list node
  *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
- typedef struct listint_s
- {
-     const int n;
-     struct listint_s *prev;
-     struct listint_s *next;
- } listint_t;
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
