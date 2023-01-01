ec0b64bb1fd6349dbe5fb97e944dc5f0087bcd97
mavix21 <marcevizcarra2109@hotmail.com>
2023-01-01 16:40:12 -0500

    Sorts an array of integers in ascending order using the quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,84 @@
 #include "sort.h"
 
 /**
+ * swap - swap the values of two int variables
+ *
+ * @x: pointer to first integer
+ * @y: pointer to second integer
+ */
+void swap(int *x, int *y)
+{
+	int temp = *x;
+
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * partition - pivots the array around the last element of the @array (high)
+ * i.e rearranges the @array in such a way all the elements before the pivot
+ * are less or equal than it, and all the elements after the pivot are greater
+ * or equal than it
+ *
+ * @array: pointer to first element of the subarray
+ * @low: index of the first element of the subarray
+ * @high: index of the last element of the subarray
+ * @size: original size of the array
+ *
+ * Return: index of the pivot element after the rearrangenment
+ */
+size_t partition(int *array, size_t low, size_t high, size_t size)
+{
+	int pivot = array[high];
+	size_t left, j;
+
+	for (j = low; j < high && array[j] < pivot; j++)
+	;
+	left = j;
+	if (j != high)
+		j++;
+
+	for (; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			swap(&array[left], &array[j]);
+			left++;
+			print_array(array, size);
+		}
+	}
+
+	if (left < high)
+	{
+		array[high] = array[left];
+		array[left] = pivot;
+		print_array(array, size);
+	}
+
+	return (left);
+}
+
+/**
+ * quick_sort_recursion - sorts the subarrays recursively
+ *
+ * @array: pointer to first element of the subarray
+ * @low: index of the first element of the subarray
+ * @high: index of the last element of the subarray
+ * @size: original size of the array
+ */
+void quick_sort_recursion(int *array, size_t low, size_t high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = partition(array, low, high, size);
+		quick_sort_recursion(array, low, pivot_index - 1, size);
+		quick_sort_recursion(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
  * quick_sort - sorts an array of integers in ascending order
  * using the quick sort algorithm
  *
@@ -9,34 +87,8 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	int pivot, temp;
-	size_t left, j;
-
 	if (array == NULL || size < 2)
 		return;
 
-	pivot = array[size - 1];
-	for (j = 0; j < size - 1 && array[j] < pivot; j++)
-	;
-	left = j;
-	if (j != size - 1)
-		j++;
-
-	for (; j < size - 1; j++)
-	{
-		if (array[j] < pivot)
-		{
-			temp = array[j];
-			array[j] = array[left];
-			array[left] = temp;
-			left++;
-			print_array(array, size);
-		}
-	}
-	array[size - 1] = array[left];
-	array[left] = pivot;
-
-	quick_sort(array, left);
-	if (left != size - 1)
-		quick_sort(array + left + 1, size - left - 1);
+	quick_sort_recursion(array, 0, size - 1, size);
 }
